fd48ecb2daf47d05010bc330497ae06b99b5ca2e
Kiryl Novik <kllnvk13@gmail.com>
2026-07-01 19:02:49 +0300

    add category page

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -6,9 +6,25 @@ export const POSTER_PATH = "https://image.tmdb.org/t/p/w185"
 
 export const PATH = {
     Main: '/',
-    Movies: '/movies',
+    Movies: '/movies/:category',
     Filtered: '/filtered-movies',
     Search: '/search',
     Favorites: '/favorites',
     NotFound: '*',
 } as const
+
+export const navItems = [
+    {to: PATH.Main, label: 'Main'},
+    {to: "movies/popular", label: 'Category Movies'},
+    {to: PATH.Filtered, label: 'Filtered Movies'},
+    {to: PATH.Search, label: 'Search'},
+    {to: PATH.Favorites, label: 'Favorites'},
+] as const
+
+export const categories = [
+    {path: '/movies/popular', label: 'Popular movies'},
+    {path: '/movies/top_rated', label: 'Top Rated movies'},
+    {path: '/movies/upcoming', label: 'Upcoming movies'},
+    {path: '/movies/now_playing', label: 'Now Playing movies'},
+] as const
+
--- src/pages/CategoryMovies/CategoryMovies.tsx
+++ src/pages/CategoryMovies/CategoryMovies.tsx
@@ -1,5 +1,44 @@
+import {categories} from "@/common/constants";
+import {useGetMoviesByCategoryQuery} from "@/features/movies/api/moviesApi";
+import type {Category} from "@/features/movies/api/moviesApi.types";
+import {MovieCard} from "@/features/movies/ui/movieCard/MovieCard";
+import {NavLink, useParams} from "react-router";
+import s from "./CategoryMovies.module.css"
+
 export const CategoryPage = () => {
+
+    const {category = "popular"} = useParams<{ category: Category }>()
+    const {data} = useGetMoviesByCategoryQuery({category, params: {page: 1}});
+
+
     return (
-        <h1>Category Page</h1>
+        <div className={s.container}>
+            <section className={s.page}>
+                <div className={s.buttonsContainer}>
+                    {categories.map(({path, label}) => (
+
+                        <NavLink
+                            key={path}
+                            className={({isActive}) =>
+                                isActive ? `${s.button} ${s.active}` : s.button
+                            }
+                            to={path}
+                        >
+                            {label}
+                        </NavLink>
+
+                    ))}
+                </div>
+                <div className={s.wrapper}>
+                    <section className={s.moviesCards}>
+                        {data?.results?.map((movie) => (
+                            <MovieCard key={movie.id} movieId={movie.id} posterUrl={movie.poster_path}
+                                       title={movie.title}
+                                       voteAverage={movie.vote_average}/>
+                        ))}
+                    </section>
+                </div>
+            </section>
+        </div>
     )
 }
